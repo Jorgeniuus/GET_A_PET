d416e77496783fab363256c156f702d76bf35eb9
Jorge Mauadie <99563509+Jorgeniuus@users.noreply.github.com>
2024-12-13 20:48:16 -0300

    feat: create feature update db

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,6 +1,10 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+
+const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-token')
+const getUserByToken = require('../helpers/get-user-by-token')
 
 module.exports = class UserController{
     static async register(req, res){
@@ -85,5 +89,103 @@ module.exports = class UserController{
             return
         }
         await createUserToken(user, req, res)
+    }
+
+    static async checkUser(req, res){
+        let currentUser
+
+        console.log(req.headers.authorization)
+
+        if(req.headers.authorization){
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'nossosecret')
+            console.log(decoded)
+            currentUser = await User.findById(decoded.id)
+            console.log(currentUser)
+            currentUser.password = undefined
+        }else{
+            currentUser = null
+        }
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res){
+        const id = req.params.id
+        const user = await User.findById(id).select('-password')
+
+        if(!user){
+            res.status(422).json({
+                message: 'Usuário não encontrado!'
+            })
+            return
+        }
+        res.status(200).json({user})
+    }
+
+    static async editUser(req, res){
+        const id = req.params.id
+
+        // check if user exists
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+        
+        const {name, email, phone, password, confirmpassword} = req.body
+
+        if(req.file){
+            user.image = req.file.filename
+        }
+
+        // validations
+        if(!name){
+            res.status(422).json({message: 'Name is required'})
+            return
+        }
+        if(!email){
+            res.status(422).json({message: 'Email is required'})
+            return
+        }
+
+        const userExists = await User.findOne({email:email})
+        
+        //check if user exists
+        if(user.email !== email && userExists){
+            res.status(422).json({
+                message: 'Please, use email'
+            })
+            return
+        }
+        user.email = email
+        
+        if(!phone){
+            res.status(422).json({message: 'Phone is required'})
+            return
+        }
+
+        user.phone = phone
+
+        if(password !== confirmpassword){
+            res.status(422).json({message: 'Password is wrong'})
+            return
+        }else if(password === confirmpassword && password != null) {
+            //creating password
+            const salt = await bcrypt.genSalt(12)
+            const passwordHash = await bcrypt.hash(password, salt)
+            user.password = passwordHash
+        }
+        
+        try{
+            //returns user updated data
+            await User.findByIdAndUpdate(
+                {_id: user._id},
+                {$set: user},
+                {new: true}
+            )
+            res.status(200).json({
+                message: 'User updated successfully!'
+            })
+        }catch(err){
+            res.status(500).json({message: err})
+            return
+        }
     }
 }
---
+++ backend/helpers/image-upload.js
@@ -0,0 +1,31 @@
+const multer = require('multer')
+const path = require('path')
+
+// destination to store the images
+const imageStorage = multer.diskStorage({
+    destination: function(req, file, callback){
+        let folder = ''
+
+        if(req.baseUrl.includes('users')){
+            folder = 'users'
+        }else if(req.baseUrl.includes('pets')){
+            folder = 'pet'
+        }
+        callback(null, `public/images/${folder}`)
+    },
+    filename: function (req, file, callback){
+        callback(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, callback){
+        if(!file.originalname.match(/\.(png|jpg)$/)){
+            return callback(new Error('Please, send only jpg or png'))
+        }
+        callback(undefined, true)
+    }
+})
+
+module.exports = {imageUpload}
